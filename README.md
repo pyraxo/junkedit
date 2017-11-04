6ae0a9578ea83d54e97299c69fc22bd9360afd12
Matt8898 <mattew8898@gmail.com>
2017-11-04 12:48:48 +0100

    Added multiedit.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Usage:
 Type list to get a list of items.
 
 Type edit and then follow the instructions to edit your inventory, the file will be saved automatically.
+Type multiedit and then follow the instructions to edit your inventory, the file will be saved automatically.
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,34 @@ fn parse_data(map: &mut HashMap<u16, String>) {
     }
 }
 
+fn write_slot(slot: usize, id: u16, amount: u32, buf: &mut Vec<u8>) {
+        let mut id_write = vec![];
+        id_write.write_u16::<BigEndian>(id).unwrap();
+        let mut amount_write = vec![];
+        amount_write.write_u32::<LittleEndian>(amount).unwrap();
+        //Write the id
+        buf[((0x1dc + (slot * 12)))] = id_write[1];
+        buf[((0x1dc + (slot * 12) + 1))] = id_write[0];
+        //Write the amount
+        buf[((0x1dc + (slot * 12) + 2))] = amount_write[0];
+        buf[((0x1dc + (slot * 12) + 3))] = amount_write[1];
+        buf[((0x1dc + (slot * 12) + 4))] = amount_write[2];
+        buf[((0x1dc + (slot * 12) + 5))] = amount_write[3];
+
+}
+
+fn read_input(prompt: &str) -> String {
+    let stdin = std::io::stdin();
+    println!("{}", prompt);
+    return stdin
+            .lock()
+            .lines()
+            .next()
+            .expect("there was no next line")
+            .expect("the line could not be read");
+
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut name_map = HashMap::new();
@@ -62,13 +90,7 @@ fn main() {
                 }
             },
             "edit" => {
-                    println!("Which slot?");
-                    let line = stdin
-                            .lock()
-                            .lines()
-                            .next()
-                            .expect("there was no next line")
-                            .expect("the line could not be read");
+                    let line = read_input("Which slot?");
                     let slot: usize = match line.parse() {
                         Ok(num) => num,
                         Err(_) => {
@@ -80,13 +102,7 @@ fn main() {
                         println!("There is no slot {}", slot);
                         continue;
                     }
-                    println!("Input the name of the item or its id in hexadecimal");
-                    let line = stdin
-                            .lock()
-                            .lines()
-                            .next()
-                            .expect("there was no next line")
-                            .expect("the line could not be read");
+                    let line = read_input("Input the name of the item or its id in hexadecimal");
                     let mut number = true;
                     let num = match u16::from_str_radix(line.as_str(), 16) {
                         Ok(v) => v,
@@ -148,21 +164,106 @@ fn main() {
                             break;
                         }
                     }
-                    let mut id_write = vec![];
-                    id_write.write_u16::<BigEndian>(id).unwrap();
-                    let mut amount_write = vec![];
-                    amount_write.write_u32::<LittleEndian>(amount).unwrap();
-                    //Write the id
-                    buf[((0x1dc + (slot * 12)))] = id_write[1];
-                    buf[((0x1dc + (slot * 12) + 1))] = id_write[0];
-                    //Write the amount
-                    buf[((0x1dc + (slot * 12) + 2))] = amount_write[0];
-                    buf[((0x1dc + (slot * 12) + 3))] = amount_write[1];
-                    buf[((0x1dc + (slot * 12) + 4))] = amount_write[2];
-                    buf[((0x1dc + (slot * 12) + 5))] = amount_write[3];
+                    write_slot(slot, id, amount, &mut buf);
                     let mut save_file = File::create(filename).unwrap();
                     save_file.write_all(&buf).unwrap();
             },
+            "multiedit" => {
+                let line = read_input("Input a comma-separated list of item ids:");
+                let id_strings: Vec<&str> = line.split(",").collect();
+                let mut ids = vec![];
+                for id in id_strings {
+                    let id = id.trim();
+                    let mut number = true;
+                    let num = match u16::from_str_radix(id, 16) {
+                        Ok(v) => v,
+                        Err(_) => {
+                            number = false;
+                            0
+                        }
+                    };
+                    let mut found = false;
+                    let mut item_id = 0;
+                    if number {
+                        item_id = match name_map.get(&num) {
+                            Some(_) => {
+                                found = true;
+                                num
+                            },
+                            None => {
+                                found = false;
+                                0
+                            }
+                        };
+                    } else {
+                        for (n, name) in name_map.iter() {
+                            let name = name.replace("\"", "").to_string();
+                            if name == id.trim() || name.to_lowercase() == id.trim().to_lowercase() {
+                                item_id = *n;
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if !found {
+                        println!("No such item: {}", id);
+                        continue;
+                    } else {
+                        ids.push(item_id);
+                    }
+                }
+                let mut amount: u32;
+                loop {
+                    let stop;
+                    let line = read_input("Enter an amount: ");
+
+                    amount = match line.parse() {
+                        Ok(num) => {
+                            stop = true;
+                            num
+                        },
+                        Err(_) => {
+                            println!("Please input a number");
+                            continue;
+                        }
+                    };
+                    if stop {
+                        break;
+                    }
+                }
+                let mut start_slot: u32;
+                loop {
+                    let stop;
+                    let line = read_input("Enter a starting slot: ");
+
+                    start_slot = match line.parse() {
+                        Ok(num) => {
+                            if (num + ids.len() as u32) > 35 {
+                                stop = false;
+                                println!("The number of items exceeds the maximum amount of slots.");
+                                0
+                            } else {
+                                stop = true;
+                                num
+                            }
+                        },
+                        Err(_) => {
+                            println!("Please input a number");
+                            continue;
+                        }
+                    };
+                    if stop {
+                        break;
+                    }
+                }
+                let mut index = 0;
+                for id in ids {
+                    write_slot((start_slot+index) as usize, id, amount, &mut buf);
+                    let mut save_file = File::create(filename).unwrap();
+                    index+=1;
+                    save_file.write_all(&buf).unwrap();
+                }
+            }
             "exit" => {
                 std::process::exit(1);
             }
